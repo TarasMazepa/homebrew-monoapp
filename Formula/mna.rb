c2f6d23744ff313e170824a7307699a1dfed7255
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2026-09-06 12:18:03 -0700

    Update download URLs for macOS and Linux versions

--- Formula/mna.rb
+++ Formula/mna.rb
@@ -4,10 +4,10 @@ class Mna < Formula
   version '0.0.5'
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/TarasMazepa/homebrew-monoapp/releases/download/#{version}/macos-arm.zip"
+    url "https://github.com/TarasMazepa/homebrew-monoapp/releases/download/cli-v#{version}/macos-arm.zip"
     sha256 '359c68a6ed3bd8d3219a38653fe70f26931632b0a2c2d869c430dd7694b72b86' # macos-arm
   elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/TarasMazepa/homebrew-monoapp/releases/download/#{version}/linux-x64.zip"
+    url "https://github.com/TarasMazepa/homebrew-monoapp/releases/download/cli-v#{version}/linux-x64.zip"
     sha256 '5edc41718ba551befa7dec433ba2ad65b98b7d8cdddcab5fa51c194f98c8a1ee' # linux-x64
   end
 
